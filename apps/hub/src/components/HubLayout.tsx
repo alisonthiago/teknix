71ab655dcce5fac1260b6f6726150b310333eaca
Alison Thiago <alisonthiago@192.168.1.25>
2026-09-12 19:48:58 -0300

    fix(hub): remover botao de precificacao da pill do header

--- apps/hub/src/components/HubLayout.tsx
+++ apps/hub/src/components/HubLayout.tsx
@@ -516,19 +516,6 @@ function HubLayoutContent() {
                   </span>
                 </button>
 
-                {/* Precificação */}
-                <button
-                  type="button"
-                  onClick={() => navigate('/hub/produtos')}
-                  className="flow-pill-btn"
-                  title="Precificação"
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/>
-                    <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/>
-                    <path d="M12 18V6"/>
-                  </svg>
-                </button>
 
                 {/* Usuário logado */}
                 <button
